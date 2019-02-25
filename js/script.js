d797bf7a94a911d1e68814d7cd6cc84b77fb2072
Savannah Milton <sjamz5555@gmail.com>
2019-02-25 04:21:47 +0000

    a

--- js/script.js
+++ js/script.js
@@ -5,16 +5,26 @@ $(document).ready(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var totalScore=totalScores();
-});
- //var finalScore;
+        var totalScore;
+        
+        var result = 0;
+        var answer1 = $("#question1").val();
+        result =result+q1(answer1);
+        
+        var answer2 = $("#question2").val();
+         result =result+q2(answer2);
+         var answer3 = $("#question3").val();
+ result =result+q3(answer3);
+}); 
+ 
 var result1 = 0;
 function q1(answer1){
+    var result=0;
     if(answer1 === "january" || "febuary" || "march" || "april" || "may" || "april"){
-        result1 = 2+finalScore;
+        return 2;
     }
     else if(answer1 === "july" || "august" || "september" || "october" || "november" || "december"){
-        result1 = 3+finalScore;
+        return 3;
     }
 }
 
@@ -24,43 +34,33 @@ function q1(answer1){
 var result2 = 0;
 function q2(answer2){
     if(answer2 === "no"){
-       return 1+finalScore;
+       return 1;
     }
     else if(answer2 === "not very"){
-     result2 = 2+finalScore;
+     return 2;
     }
     else if(answer2 === "very"){
-        result2 = 3+finalScore;
+        return 3;
         }
 }
-var answer2;
-q2(answer2);
 
 var result3 = 0;
 function q3(answer3){
     if(answer3.length>5){
-        result3 = 5+finalScore;
+        return 5;
     }
     else if(answer3.length<5){
-     result3 = 10+finalScore;
+     return 10;
     }
 }
 
-function totalScores(){
-$("#result").click(function(){
-  
-q1(answer1);
-q2(answer2);
-q3(answer3);
-var q1Result = $("#question1").val();
-var q2Result = $("#question2").val();
-var q3Result = $("#question3").val();
 
-if(finalScore > 10){
+/*global result*/
+if(result > 10){
         $("btext").val("You are a rubber band ball! You're colorful bouncy and made of many happy layers!");
     }
-    else if(finalScore<10){
+    else{
     $("btext").val("You are  a paper clip! Your a nosieless sedentary creature who comes in handy when the stapler is otherwise occupied!");
     }
-});
+
 });
